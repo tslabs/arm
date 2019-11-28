2c6cda0fafafa738cc9d067f58fd23235ce58fc5
TSL <tslabs.info@gmail.com>
2019-11-28 22:06:33 +0200

    Tools: changed types

--- AYX-32/Tool/fw_uart/fw_uart/stdafx.h
+++ AYX-32/Tool/fw_uart/fw_uart/stdafx.h
@@ -12,24 +12,34 @@
 #include <memory.h>
 #include <tchar.h>
 #include <sys/stat.h>
+#include <stdint.h>
 
-typedef unsigned long long  U64;
-typedef unsigned long       U32;
-typedef unsigned short      U16;
-typedef unsigned char       U8;
-typedef unsigned long long  u64;
-typedef unsigned long       u32;
-typedef unsigned short      u16;
-typedef unsigned char       u8;
+typedef uint8_t  u8;
+typedef uint16_t u16;
+typedef uint32_t u32;
+typedef uint64_t u64;
+typedef uint8_t  U8;
+typedef uint16_t U16;
+typedef uint32_t U32;
+typedef uint64_t U64;
 
-typedef signed long long    I64;
-typedef signed long         I32;
-typedef signed short        I16;
-typedef signed char         I8;
-typedef signed long long    i64;
-typedef signed long         i32;
-typedef signed short        i16;
-typedef signed char         i8;
+typedef int8_t  i8;
+typedef int16_t i16;
+typedef int32_t i32;
+typedef int64_t i64;
+typedef int8_t  I8;
+typedef int16_t I16;
+typedef int32_t I32;
+typedef int64_t I64;
+
+typedef int8_t  s8;
+typedef int16_t s16;
+typedef int32_t s32;
+typedef int64_t s64;
+typedef int8_t  S8;
+typedef int16_t S16;
+typedef int32_t S32;
+typedef int64_t S64;
 
 #define max(a,b)  (((a) > (b)) ? (a) : (b))
 #define min(a,b)  (((a) < (b)) ? (a) : (b))
